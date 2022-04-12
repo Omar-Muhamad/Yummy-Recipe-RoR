ab41257e1a9b41efe88f566a5b937f761edacef1
Anjacodes <anja_schmidt7@posteo.de>
2022-04-12 17:03:31 -0500

    Fix Linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,6 +6,7 @@ class RecipesController < ApplicationController
   # GET /recipes or /recipes.json
   def index
     return unless user_signed_in?
+
     @user = User.find(current_user.id)
     @recipes = Recipe.where(user_id: @user.id)
   end

aeb32d870d9f0ce239a906170777c6ee68666776
Anjacodes <anja_schmidt7@posteo.de>
2022-04-13 11:14:07 -0500

    Fix Linters

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -25,7 +25,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to foods_url, notice: "Food was successfully created." }
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -60,10 +60,11 @@ class FoodsController < ApplicationController
 
   private
 
-    # Only allow a list of trusted parameters through.
-    def food_params
-      params.require(:food).permit(:name, :measurement_unit, :price)
-    end
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_food
     @food = Food.find(params[:id])

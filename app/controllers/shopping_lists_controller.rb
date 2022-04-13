ef168388001c1a202dcdac121fdec54324982a09
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-04-14 01:07:27 +0200

    Fix linters Add and rspec gem

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,11 +1,11 @@
 class ShoppingListsController < ApplicationController
-    load_and_authorize_resource
+  load_and_authorize_resource
 
-    def index
-      @recipes = Recipe.where(user_id: current_user.id)
-      @recipe_foods = []
-      @recipes.each do |recipe|
-        @recipe_foods << recipe.recipe_foods
-      end
+  def index
+    @recipes = Recipe.where(user_id: current_user.id)
+    @recipe_foods = []
+    @recipes.each do |recipe|
+      @recipe_foods << recipe.recipe_foods
     end
+  end
 end
